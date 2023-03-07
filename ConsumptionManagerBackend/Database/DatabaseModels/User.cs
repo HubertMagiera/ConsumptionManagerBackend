750f970eb19b56efbe93f15afa1cc21f96f12725
HubertMagiera <h.magiera01@gmail.com>
2023-03-07 19:10:42 +0100

    added user class

---
+++ ConsumptionManagerBackend/Database/DatabaseModels/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsumptionManagerBackend.Database.DatabaseModels
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public string user_surname { get; set; }
+        public int electricity_tariff_id { get; set; }
+        public int user_credentials_id { get; set; }
+    }
+}
--- ConsumptionManagerBackend/Database/EnergySaverDbContext.cs
+++ ConsumptionManagerBackend/Database/EnergySaverDbContext.cs
@@ -11,11 +11,14 @@ namespace ConsumptionManagerBackend.Database
         }
         //this will be mapped to database class
         public DbSet<UserCredentials> user_credentials { get; set; }
+        public DbSet<User> user { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //specifying that entities have primary keys and indicating them
             modelBuilder.Entity<UserCredentials>().HasKey(key => key.user_credentials_id);
+
+            modelBuilder.Entity<User>().HasKey(key => key.user_id);
         }
     }
 }
