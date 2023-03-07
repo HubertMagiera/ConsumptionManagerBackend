77a41dea85e9fa390df68ece1c3eaae7bd6f2321
HubertMagiera <h.magiera01@gmail.com>
2023-03-07 12:39:18 +0100

    created user credentials dto class

---
+++ ConsumptionManagerBackend/DtoModels/UserCredentialsDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsumptionManagerBackend.DtoModels
+{
+    public class UserCredentialsDto
+    {
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string UserPassword { get; set; }
+    }
+}
