82af56b22473539fbd886f7a4e314a12156d3179
HubertMagiera <h.magiera01@gmail.com>
2023-03-09 18:51:16 +0100

    created mapping profiles for energy supplier, electricity tariff and tariff details classes

--- ConsumptionManagerBackend/MappingProfile.cs
+++ ConsumptionManagerBackend/MappingProfile.cs
@@ -2,6 +2,7 @@
 using ConsumptionManagerBackend.Database.DatabaseModels;
 using ConsumptionManagerBackend.DtoModels;
 using ConsumptionManagerBackend.DtoModels.ModelsForAdding;
+using ConsumptionManagerBackend.DtoModels.ModelsForViewing;
 
 namespace ConsumptionManagerBackend
 {
@@ -21,6 +22,22 @@ namespace ConsumptionManagerBackend
                 .ForMember(destination => destination.user_surname, map => map.MapFrom(baseClass => baseClass.UserSurname))
                 .ForMember(destination => destination.user_name, map => map.MapFrom(baseClass => baseClass.UserName))
                 .ForMember(destination => destination.user_credentials_id, map => map.MapFrom(baseClass => baseClass.UserCredentialsId));
+
+            CreateMap<EnergySupplier, EnergySupplierDto>()
+                .ForMember(destination => destination.EnergySupplierName, map => map.MapFrom(baseClass => baseClass.energy_supplier_name))
+                .ForMember(destination => destination.tariffs, map => map.MapFrom(baseClass => baseClass.tariffs));
+
+            CreateMap<ElectricityTariff, ElectricityTariffDto>()
+                .ForMember(destination => destination.TariffName, map => map.MapFrom(baseClass => baseClass.tariff_name))
+                .ForMember(destination => destination.TariffDescription, map => map.MapFrom(baseClass => baseClass.tariff_description))
+                .ForMember(destination => destination.EnergySupplierName, map => map.MapFrom(baseClass => baseClass.energy_supplier.energy_supplier_name))
+                .ForMember(destination => destination.TariffDetails, map => map.MapFrom(baseClass => baseClass.tariff_details));
+
+            CreateMap<TariffDetails, TariffDetailsDto>()
+                .ForMember(destination => destination.DayOfWeekName, map => map.MapFrom(baseClass => baseClass.day_of_week.day_name))
+                .ForMember(destination => destination.StartTime, map => map.MapFrom(baseClass => baseClass.start_time))
+                .ForMember(destination => destination.EndTime, map => map.MapFrom(baseClass => baseClass.end_time))
+                .ForMember(destination => destination.PricePerKwh, map => map.MapFrom(baseClass => baseClass.price_per_kwh));
         }
 
 
