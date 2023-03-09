f2907cb6c847aa7ade3d12feb39c90cf94080ebb
HubertMagiera <h.magiera01@gmail.com>
2023-03-09 17:54:26 +0100

    energy supplier class from db

---
+++ ConsumptionManagerBackend/Database/DatabaseModels/EnergySupplier.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsumptionManagerBackend.Database.DatabaseModels
+{
+    public class EnergySupplier
+    {
+        public int energy_supplier_id { get; set; }
+
+        public string energy_supplier_name { get; set; }
+    }
+}
