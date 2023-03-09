e9f3b26640bf9dc1ccddd4505165acb1a976dbfb
HubertMagiera <h.magiera01@gmail.com>
2023-03-09 18:07:22 +0100

    day of week class from db

---
+++ ConsumptionManagerBackend/Database/DatabaseModels/DayOfWeek.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsumptionManagerBackend.Database.DatabaseModels
+{
+    public class DayOfWeek
+    {
+        public int day_of_week_id { get; set; }
+
+        public string day_name { get; set; }
+    }
+}
