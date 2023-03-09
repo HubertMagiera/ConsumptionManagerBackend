fbdcd86f998f1442a41ce7a8634b0feb2d008bb3
HubertMagiera <h.magiera01@gmail.com>
2023-03-09 18:06:19 +0100

    tariff details class from db

---
+++ ConsumptionManagerBackend/Database/DatabaseModels/TariffDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsumptionManagerBackend.Database.DatabaseModels
+{
+    public class TariffDetails
+    {
+        public int tariff_details_id { get; set; }
+
+        public int electricity_tariff_if { get; set; }
+
+        public int day_of_week_id { get; set; }
+
+        public DateTime start_time { get; set; }
+
+        public DateTime end_time { get; set;}
+
+        public double price_per_kwh { get; set; }
+    }
+}
