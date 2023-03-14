7701ea8c1a369428acb5ac877026bfcfe2b31052
HubertMagiera <h.magiera01@gmail.com>
2023-03-14 19:15:55 +0100

    dependency injection into device service class

--- ConsumptionManagerBackend/Services/DeviceService.cs
+++ ConsumptionManagerBackend/Services/DeviceService.cs
@@ -1,10 +1,20 @@
-﻿using ConsumptionManagerBackend.DtoModels.ModelsForSearching;
+﻿using AutoMapper;
+using ConsumptionManagerBackend.Database;
+using ConsumptionManagerBackend.DtoModels.ModelsForSearching;
 using ConsumptionManagerBackend.DtoModels.ModelsForViewing;
 
 namespace ConsumptionManagerBackend.Services
 {
     public class DeviceService : IDeviceService
     {
+        private readonly EnergySaverDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DeviceService(EnergySaverDbContext context, IMapper mapper)
+        {
+            _context= context;
+            _mapper= mapper;
+        }
         public List<string> GetCategories()
         {
             throw new NotImplementedException();
